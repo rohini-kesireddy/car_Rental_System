5b1de4a351c6a59f3e17923c81b28d3403fe98e0
rohini-kesireddy <78852450+rohini-kesireddy@users.noreply.github.com>
2021-03-20 16:29:01 +0530

    Add files via upload

---
+++ Registration/src/main/java/com/ltts/repo/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ltts.repo;
+import com.ltts.model.User;
+import com.ltts.repo.UserRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	@Query("SELECT u FROM User u WHERE u.email = ?1")
+	public User findByEmail(String email);
+	
+}
---
+++ Registration/src/main/java/com/ltts/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.ltts.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.ltts.model.CustomUserDetails;
+import com.ltts.model.User;
+import com.ltts.repo.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepo.findByEmail(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		return new CustomUserDetails(user);
+	}
+
+}
