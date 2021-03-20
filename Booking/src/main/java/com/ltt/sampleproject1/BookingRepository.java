f38381eb9436479ad9796f030cceb69f527d342c
rohini-kesireddy <78852450+rohini-kesireddy@users.noreply.github.com>
2021-03-20 16:27:05 +0530

    Add files via upload

---
+++ Booking/src/main/java/com/ltt/sampleproject1/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.ltt.sampleproject1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+ 
+}
+
+
